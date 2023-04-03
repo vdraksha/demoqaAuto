ec240cff12ccef3f4f4f6f7d004c8bc28fa37b86
vdraksha <vdraksha@yandex.ru>
2023-04-03 11:36:30 +0500

    Провальный тест исправлен. Chrome -> Firefox.

--- conftest.py
+++ conftest.py
@@ -1,20 +1,11 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(scope='function')
-def chrome_driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.maximize_window()
-    yield driver
-    driver.quit()
-
-
-@pytest.fixture(scope='function')
-def firefox_driver():
-    driver = webdriver.firefox(GeckoDriverManager().install())
+def driver():
+    driver = webdriver.Firefox()
+    #driver.set_page_load_timeout(30)
     driver.maximize_window()
     yield driver
     driver.quit()
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,14 +1,9 @@
 import time
 from pages.base_page import BasePage
-from conftest import firefox_driver
 
 
 def test(driver):
-    page = BasePage(firefox_driver, 'ya.ru')
+    page = BasePage(driver, 'https://ya.ru/')
     page.open_url()
     time.sleep(3)
-
-"""
-Запусти test и отдебаж! Время разбора провальных тестов! Бугага
-"""
 
