58743487cd4a083aaa4dee48308960d613a0e1eb
vdraksha <vdraksha@yandex.ru>
2023-04-03 13:37:23 +0500

    Тест на text-box. Добавлено считывание output.

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -12,4 +12,9 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys("here is here")
         self.element_is_visible(self.locators.SUBMIT).click()
 
-
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_address, permanent_address
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -10,4 +10,9 @@ class TestElements:
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open_url()
             text_box_page.fill_all_fields()
-            time.sleep(5)
+            out_full_name, out_email, out_current_address, out_permanent_address = text_box_page.check_filled_form()
+            print(out_full_name)
+            print(out_email)
+            print(out_current_address)
+            print(out_permanent_address)
+
