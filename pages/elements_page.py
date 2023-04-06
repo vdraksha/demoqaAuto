40ce986b2c6d40ba06057544fcfdb5cc6bc3c13e
vdraksha <vdraksha@yandex.ru>
2023-04-06 11:22:45 +0500

    Тест на text-box. Генерация данных.

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,3 +1,4 @@
+from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -6,11 +7,17 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys("Ivan Pirogov")
-        self.element_is_visible(self.locators.EMAIL).send_keys("ivapir1703@bestmail.com")
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys("here")
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys("here is here")
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+        return full_name, email, current_address, permanent_address
 
     def check_filled_form(self):
         full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -9,10 +9,6 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open_url()
-            text_box_page.fill_all_fields()
-            out_full_name, out_email, out_current_address, out_permanent_address = text_box_page.check_filled_form()
-            print(out_full_name)
-            print(out_email)
-            print(out_current_address)
-            print(out_permanent_address)
-
+            input_data = text_box_page.fill_all_fields()  # full_name, email, current_address, permanent_address
+            output_data = text_box_page.check_filled_form()  # full_name, etc. с поля вывода
+            assert input_data == output_data  # Если сравнивать каждое поле отдельно, то локализовать ошибку легче
